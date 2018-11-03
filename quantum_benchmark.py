Fixing that quantumsim has no fidelity calculation

quantum_benchmark.py
@@ -868,7 +868,7 @@ class SimBench(object):
 
     def mean_fidelity(self):
 
-        return np.mean(self.fidelity_registry)
+        return np.mean(self.fidelity_registry) if self.fidelity_registry else -1
 
     def mean_success(self):
 