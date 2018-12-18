bfc5018ea3298780628e51ff3a2c8c3d61d6b920
Gonaco <danielmoremanza@gmail.com>
2018-12-18 20:14:37 +0100

    Completing a bit more the data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -16,19 +16,31 @@ def extract_db_main_info(db_path):
 
     query = "SELECT DISTINCT HardwareBenchs.N_gates, HardwareBenchs.N_swaps, depth, prob_succs, mean_f, q_vol FROM SimulationsInfo LEFT JOIN HardwareBenchs ON algorithm=HardwareBenchs.id LEFT JOIN Results ON result=Results.id LEFT JOIN Experiments ON experiment=Experiments.id LEFT JOIN Benchmarks ON HardwareBenchs.benchmark=Benchmarks.id LEFT JOIN Configurations ON configuration=Configurations.id;"
     cursor.execute(query)
-    return cursor.fetchone()
+    return cursor.fetchall()
 
-# def store_db_main_info(main_info):
+
+def store_db_main_info(N_gates, N_swaps, depth, prob_succs, mean_f, q_vol):
+
+    data_frame = pd.DataFrame({"N_gates": N_gates, "N_swaps": N_swaps, "depth": depth,
+                               "prob_succs": prob_succs, "mean_f": mean_f, "q_vol": q_vol})
+
+    return data_frame
+
 
 # def plot_relation(data):
 #     plt.scatter(data1, data2)
 #     plt.savefig("")
 
-
 # def clean_data_frame(data_frame):
 #     # I use Quantum Volume as the harder variable to be randomly repeated
 #     data_frame.drop_duplicates(subset=['v_q'], keep='first')
 
+N_gates = []
+N_swaps = []
+depth = []
+prob_succs = []
+mean_f = []
+q_vol = []
 
 for i in range(4):
 
@@ -36,6 +48,12 @@ for i in range(4):
         i=i+1)
 
     bench_info = extract_db_main_info(db_path)
-    print(bench_info)
+    for b_i in bench_info:
+        N_gates.append(b_i[0])
+        N_swaps.append(b_i[1])
+        depth.append(b_i[2])
+        prob_succs.append(b_i[3])
+        mean_f.append(b_i[4])
+        q_vol.append(b_i[5])
 
     # pearsonr(x, y)
