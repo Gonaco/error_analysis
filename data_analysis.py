4a74b71e31c915d2fbec9d928b55df5237824183
Gonaco <danielmoremanza@gmail.com>
2018-12-18 17:50:13 +0100

    Completing a bit more the data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-white')
 
 from scipy.stats import pearsonr
+import pandas as pd
 
 
 def extract_db_main_info(db_path):
@@ -13,14 +14,20 @@ def extract_db_main_info(db_path):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
 
-    query = "SELECT HardwareBenchs.N_gates, HardwareBenchs.N_swaps, depth, prob_succs, mean_f, q_vol FROM SimulationsInfo LEFT JOIN HardwareBenchs ON algorithm=HardwareBenchs.id LEFT JOIN Results ON result=Results.id LEFT JOIN Experiments ON experiment=Experiments.id LEFT JOIN Benchmarks ON HardwareBenchs.benchmark=Benchmarks.id LEFT JOIN Configurations ON configuration=Configurations.id;"
+    query = "SELECT DISTINCT HardwareBenchs.N_gates, HardwareBenchs.N_swaps, depth, prob_succs, mean_f, q_vol FROM SimulationsInfo LEFT JOIN HardwareBenchs ON algorithm=HardwareBenchs.id LEFT JOIN Results ON result=Results.id LEFT JOIN Experiments ON experiment=Experiments.id LEFT JOIN Benchmarks ON HardwareBenchs.benchmark=Benchmarks.id LEFT JOIN Configurations ON configuration=Configurations.id;"
     cursor.execute(query)
     return cursor.fetchone()
 
+# def store_db_main_info(main_info):
 
 # def plot_relation(data):
 #     plt.scatter(data1, data2)
 #     plt.savefig("")
+
+
+# def clean_data_frame(data_frame):
+#     # I use Quantum Volume as the harder variable to be randomly repeated
+#     data_frame.drop_duplicates(subset=['v_q'], keep='first')
 
 
 for i in range(4):
